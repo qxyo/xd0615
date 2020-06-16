4e74c057910e74afeee04cd77a4e2cb24a4bbb15
qxyo <1046829468@qq.com>
2020-06-16 21:33:51 +0800

    提交第二次作业

---
+++ lessons2/led.c
@@ -0,0 +1,50 @@
+void setup()
+{
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(0, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(0, LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(1, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(1, LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+ digitalWrite(2, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(2, LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+   digitalWrite(3, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(3, LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(4, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(4, LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(5, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(5, LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(6, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(6, LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(7, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  digitalWrite(7, LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+  
+  
+
+}
